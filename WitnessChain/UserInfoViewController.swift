a489c3c57ec96977f716280dc77f15e3e17a069b
Harshal Singh <harshalsingh@Harshals-MacBook-Pro.local>
2018-04-19 18:34:08 -0400

    testing key gen

--- WitnessChain/UserInfoViewController.swift
+++ WitnessChain/UserInfoViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 import Firebase
+import Alamofire
+
 
 class UserInfoViewController: UIViewController {
     
     var ref: DatabaseReference!
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     let user = Auth.auth().currentUser
     @IBOutlet weak var PublicKeyField: UITextField!
@@ -26,6 +30,24 @@ class UserInfoViewController: UIViewController {
         // set public key field, private key field, address field to something default
         
         // TODO: ACTUALLY POPULATE THESE FIELDS WITH REAL STUFF
+        let url = URL(string: self.appDelegate.baseUrl + "/address")! // calling the address function from the API
+        let parameters = ["password": "harshalkevin"]
+        
+        
+        Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+                //                                self.dismiss(animated: true, completion: nil) // dismiss the image preview somehow
+            }
+        }
         
         PublicKeyField.text = "something random"
         
